3c79436159dbe6806bce6aef971447fb7700dcf6
JugHead <35992294+ERICAntoine@users.noreply.github.com>
2025-03-08 22:54:20 +0100

    Remove unused import

--- test/vlc_player_x_test.dart
+++ test/vlc_player_x_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vlc_player_x/src/vlc_player_x.dart';
-
 void main() {
   test('adds one to input values', () {
     // final calculator = Calculator();
